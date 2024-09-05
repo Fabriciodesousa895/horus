76f9926cc944e9d340415918754e2c069ba738fc
Fabricio Evangelista <sousafabricio583@gmail.com>
2024-09-04 22:29:54 -0300

    natureza

--- public/js/FormClass/ActionForm.js
+++ public/js/FormClass/ActionForm.js
@@ -92,6 +92,7 @@ class ActionForm{
                 let td = element.querySelectorAll('td')
                 td.forEach((tdcorrent, index) => {
                   if (index == 0) {
+                    alert(tdcorrent)
                     let data = {
                       sql:`BEGIN DELETE FROM ${this.TableName} WHERE ${PkRegistro} = :PkRegistro; COMMIT; END;`,
                       binds:{PkRegistro:tdcorrent.textContent},
--- public/js/cadastro/naturezas.js
+++ public/js/cadastro/naturezas.js
@@ -1,7 +1,24 @@
 import { ActionForm } from "../FormClass/ActionForm.js";         
 import { SalvaFiltro } from "../Class/Filtro.js";         
 import { Tabela } from "../Class/Tabela.js";         
+import { Ajax } from "../Class/Ajax.js";         
 new ActionForm('formnatureza','NATUREZA').Insert();
+new ActionForm('formnatureza','NATUREZA').delete('IdPknat');
 new ActionForm('form_upd_natureza','NATUREZA').Update('IdPknat')
-let ObjetoValue = new Tabela().InputsValues(['ID_NAT','NOME_NAT'])
-new SalvaFiltro(381,ObjetoValue.ID_NAT,ObjetoValue.NOME_NAT,'','','','','','','','')
+
+//Usando o filtro
+document.getElementById('filtro').addEventListener('click',(e)=>{
+    e.preventDefault()
+ 
+    let ObjetoValue = new Tabela().InputsValues(['ID_NAT','NOME_NAT'])
+    new SalvaFiltro(381,ObjetoValue.ID_NAT,ObjetoValue.NOME_NAT,'','','','','','','','');
+    let data ={
+      sql:`SELECT FILTRA_NATUREZA(:ID_NAT,:NOME_NAT) FROM DUAL`,
+      binds:ObjetoValue,
+      mensagem_error: "Error ao consultar registro",
+      USU_LOGADO: false
+    };
+   new Ajax('/select/universal',data).RequisicaoAjax(false).then((dados)=>{
+   new Tabela('Naturezas').InseriRegistros(dados)
+   })
+  })
